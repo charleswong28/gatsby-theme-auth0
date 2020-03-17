2c6261e2dc38def4843df63f58a1e2108ad0db5b
Charles Wong <chung.triniti@gmail.com>
2020-03-17 16:36:06 +0800

    Fix static build

--- gatsby-theme-auth0/src/auth/service.ts
+++ gatsby-theme-auth0/src/auth/service.ts
@@ -18,7 +18,7 @@ class Auth {
 
   public sessionStateCallback = (_state: SessionState) => {};
 
-  private auth0 = process.env.AUTH0_DOMAIN
+  private auth0 = isBrowser && process.env.AUTH0_DOMAIN
     ? new auth0.WebAuth(config)
     : undefined;
 
